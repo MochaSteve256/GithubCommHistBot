d74e8944721cd5319f1e8c94883bcf9d1117566f
Adrian Steyer <adrian.steyer@posteo.de>
2023-12-23 00:02:01 +0100

    started main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import datetime
+import schedule
+import time
+from PIL import Image
+
+img = Image.open("mochasteve.png")
+pixels = img.load()
+
+def commit():
+    #modify a file
+    
+    #commit changes
+    
+    pass
+
+while datetime.datetime.now().year != 2024:
+    time.sleep(3600)
+
+while datetime.datetime.now().year == 2024:
+    pass
