f899eb10357032a8faa71bfe17578f7c35117ccf
Adrian Steyer <adrian.steyer@posteo.de>
2024-01-15 13:01:43 +0100

    probably finished code

--- main.py
+++ main.py
@@ -9,13 +9,37 @@ pixels = img.load()
 
 def commit():
     #modify a file
-    
+    filedata = ""
+    with open("modifiedFile.txt", "r") as file:
+        filedata = file.read()
+    with open("modifiedFile.txt", "w") as file:
+        file.write(filedata + "\n" + str(datetime.datetime.now()))
     #commit changes
-    
+    os.system("git add modifiedFile.txt")
+    os.system("git commit -m \"Automated commit at " + str(datetime.datetime.now()) + "\"")
+    os.system("git push")
     pass
 
+def dailyTask():
+    #check if there's a black pixel for the current day
+    ##get calendar week
+    week = datetime.datetime.now().isocalendar()[1] - 1
+    ##get week day
+    weekday = datetime.datetime.now().weekday() + 1
+    if weekday == 7:#github week starts on sunday
+        weekday = 0
+        week += 1
+    ##check if there's a black pixel
+    if pixels[week, weekday] == (0, 0, 0, 255):
+        commit()
+
+#set up daily task
+schedule.every().day.at("12:00").do(dailyTask)
+
 while datetime.datetime.now().year != 2024:
+    print("Waiting for 2024...")
     time.sleep(3600)
 
 while datetime.datetime.now().year == 2024:
-    pass
+    schedule.run_pending()
+    time.sleep(3600)
---
+++ test.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import datetime
+
+img = Image.open("mochasteve.png")
+pixels = img.load()
+
+week = datetime.datetime.now().isocalendar()[1] - 1
+weekday = datetime.datetime.now().weekday() + 1
+if weekday == 7:
+    weekday = 0
+    week += 1
+
+print("week: " + str(week))
+print("weekday: " + str(weekday))
+
+if pixels[week, weekday] == 1:
+    print("black")
+else:
+    print("not black")
